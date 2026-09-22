4d7129b3d1b937eadb0cc5f95d096f352459558f
Dharunika Natarajan <dharunika0708@gmail.com>
2026-09-22 09:05:22 +0530

    Solve: [Q0.21] Basic_level_1_Datatypes_1 (Medium) - PYTHON3 | SEED-IT

---
+++ Problems/general/q0-21-basic-level-1-datatypes-1/solution.py
@@ -0,0 +1,15 @@
+"""
+Problem: Q0.21 - Basic_level_1_Datatypes_1
+Category: General
+Difficulty: Medium
+Platform: SEED-IT Platform (https://seed-it.com)
+Date Solved: 2026-09-22
+Language: python3
+Test Cases: 30 / 30 Passed (100%)
+"""
+
+# your code goes here
+num1 , num2 = map(int,input().split())
+print(f"{num1:05d}")
+
+print(f"{num2:5d}")
