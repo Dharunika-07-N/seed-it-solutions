300ba505c9517002af9c6f328584e6df92cea980
Dharunika Natarajan <dharunika0708@gmail.com>
2026-09-22 09:37:32 +0530

    Solve: [Q0.22] Basic_level_1_Datatypes_2 (Medium) - PYTHON3 | SEED-IT

---
+++ Problems/general/q0-22-basic-level-1-datatypes-2/solution.py
@@ -0,0 +1,13 @@
+"""
+Problem: Q0.22 - Basic_level_1_Datatypes_2
+Category: General
+Difficulty: Medium
+Platform: SEED-IT Platform (https://seed-it.com)
+Date Solved: 2026-09-22
+Language: python3
+Test Cases: 30 / 30 Passed (100%)
+"""
+
+# your code goes here
+num = float(input())
+print(f"{num:e}")
