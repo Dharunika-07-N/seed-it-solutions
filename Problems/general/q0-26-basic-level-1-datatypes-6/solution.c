dcaf1697e1a8e8f7810492cc66b956b96d30851e
Dharunika Natarajan <dharunika0708@gmail.com>
2026-09-22 19:00:01 +0530

    Solve: [Q0.26] Basic_level_1_Datatypes_6 (Medium) - C | SEED-IT

---
+++ Problems/general/q0-26-basic-level-1-datatypes-6/solution.c
@@ -0,0 +1,18 @@
+/**
+ * Problem: Q0.26 - Basic_level_1_Datatypes_6
+ * Category: General
+ * Difficulty: Medium
+ * Platform: SEED-IT Platform (https://seed-it.com)
+ * Date Solved: 2026-09-22
+ * Language: c
+ * Test Cases: 30 / 30 Passed (100%)
+ */
+
+#include <stdio.h>
+
+int main() {
+    char str[50];
+    scanf("%[^a-z]",str);
+    printf("%s",str);
+    return 0;
+}
