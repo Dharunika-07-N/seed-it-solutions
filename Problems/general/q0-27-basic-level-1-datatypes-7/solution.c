434acde07acda0f3f0808913ae0baef063be6024
Dharunika Natarajan <dharunika0708@gmail.com>
2026-09-22 19:02:01 +0530

    Solve: [Q0.27] Basic_level_1_Datatypes_7 (Medium) - C | SEED-IT

---
+++ Problems/general/q0-27-basic-level-1-datatypes-7/solution.c
@@ -0,0 +1,18 @@
+/**
+ * Problem: Q0.27 - Basic_level_1_Datatypes_7
+ * Category: General
+ * Difficulty: Medium
+ * Platform: SEED-IT Platform (https://seed-it.com)
+ * Date Solved: 2026-09-22
+ * Language: c
+ * Test Cases: 30 / 30 Passed (100%)
+ */
+
+#include <stdio.h>
+
+int main() {
+    char str[50];
+    scanf("%[^1-100]",str);
+    printf("%s",str);
+    return 0;
+}
