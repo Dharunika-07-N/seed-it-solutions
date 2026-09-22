9369fe98f319a6245f4313d555df9fc5302ebff9
Dharunika Natarajan <dharunika0708@gmail.com>
2026-09-22 18:25:47 +0530

    Solve: [Q0.23] Basic_level_1_Datatypes_3 (Medium) - C | SEED-IT

---
+++ Problems/general/q0-23-basic-level-1-datatypes-3/solution.c
@@ -0,0 +1,18 @@
+/**
+ * Problem: Q0.23 - Basic_level_1_Datatypes_3
+ * Category: General
+ * Difficulty: Medium
+ * Platform: SEED-IT Platform (https://seed-it.com)
+ * Date Solved: 2026-09-22
+ * Language: c
+ * Test Cases: 30 / 30 Passed (100%)
+ */
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    scanf("%d",&num);
+    printf("%zu",sizeof(num));
+    return 0;
+}
