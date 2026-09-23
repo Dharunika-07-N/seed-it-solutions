383656e7721467620ec8622f01b32e51f34de921
Dharunika Natarajan <dharunika0708@gmail.com>
2026-09-23 21:25:18 +0530

    Solve: [Q0.33] Basic_Level_0_Conditional_Statements_2 (Medium) - PYTHON3 | SEED-IT

---
+++ Problems/general/q0-33-basic-level-0-conditional-statements-2/solution.py
@@ -0,0 +1,16 @@
+"""
+Problem: Q0.33 - Basic_Level_0_Conditional_Statements_2
+Category: General
+Difficulty: Medium
+Platform: SEED-IT Platform (https://seed-it.com)
+Date Solved: 2026-09-23
+Language: python3
+Test Cases: 30 / 30 Passed (100%)
+"""
+
+# your code goes here
+num1 , num2 = map(int,input().split())
+if num1 == num2:
+    print("Equal")
+else:
+    print("Not Equal")
