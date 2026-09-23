c1445212ad6540e5c43ba598a0f2d13ff52dc48c
Dharunika Natarajan <dharunika0708@gmail.com>
2026-09-23 21:15:02 +0530

    Solve: [Q0.34] Basic_Level_0_Conditional_Statements_3 (Medium) - PYTHON3 | SEED-IT

---
+++ Problems/general/q0-34-basic-level-0-conditional-statements-3/solution.py
@@ -0,0 +1,18 @@
+"""
+Problem: Q0.34 - Basic_Level_0_Conditional_Statements_3
+Category: General
+Difficulty: Medium
+Platform: SEED-IT Platform (https://seed-it.com)
+Date Solved: 2026-09-23
+Language: python3
+Test Cases: 30 / 30 Passed (100%)
+"""
+
+# your code goes here
+num = int(input())
+if num < 0:
+    print("Negative")
+elif num > 0 :
+    print("Positive")
+else :
+    print("Zero")
