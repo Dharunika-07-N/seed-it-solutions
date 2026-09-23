1f834d80bc8e1e53751c44a7956b533aeec14cfb
Dharunika Natarajan <dharunika0708@gmail.com>
2026-09-23 21:42:28 +0530

    Solve: [Q0.35] Basic_Level_0_Conditional_Statements_4 (Medium) - PYTHON3 | SEED-IT

---
+++ Problems/general/q0-35-basic-level-0-conditional-statements-4/solution.py
@@ -0,0 +1,16 @@
+"""
+Problem: Q0.35 - Basic_Level_0_Conditional_Statements_4
+Category: General
+Difficulty: Medium
+Platform: SEED-IT Platform (https://seed-it.com)
+Date Solved: 2026-09-23
+Language: python3
+Test Cases: 30 / 30 Passed (100%)
+"""
+
+# your code goes here
+num = int(input())
+if num % 3 == 0:
+    print("The number is divisible by 3")
+else:
+    print("The number is not divisible by 3 and gives a remainder 1")
