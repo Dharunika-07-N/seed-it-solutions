be7e4eaffdaba106795c9932665a5d82fdfb1c47
Dharunika Natarajan <dharunika0708@gmail.com>
2026-09-23 21:49:52 +0530

    Solve: [Q0.28] Basic_level_1_Datatypes_8 (Medium) - PYTHON3 | SEED-IT

--- Problems/general/q0-28-basic-level-1-datatypes-8/solution.py
+++ Problems/general/q0-28-basic-level-1-datatypes-8/solution.py
@@ -3,13 +3,11 @@ Problem: Q0.28 - Basic_level_1_Datatypes_8
 Category: General
 Difficulty: Medium
 Platform: SEED-IT Platform (https://seed-it.com)
-Date Solved: 2026-09-22
+Date Solved: 2026-09-23
 Language: python3
 Test Cases: 30 / 30 Passed (100%)
 """
 
-# your code goes here
-num1 , num2 , num3 = map(int,input().split())
-maxx = num3
-maxx = num1 if num1 > num2 and num1 > num3 else num2
+num1,num2,num3 = map(int,input().split())
+maxx = num1 if num1> num2 and num1>num3 else num2 if num2>num1 and num2>num3 else num3
 print(maxx)
