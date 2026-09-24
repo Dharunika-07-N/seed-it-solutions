fd46e47d0264fccf4d08e5efba7130acd0ece09c
Dharunika Natarajan <dharunika0708@gmail.com>
2026-09-24 23:19:13 +0530

    Solve: [Q0.50] Basic_Level_1_Conditional_Statements_9 (Medium) - PYTHON3 | SEED-IT

---
+++ Problems/general/q0-50-basic-level-1-conditional-statements-9/solution.py
@@ -0,0 +1,13 @@
+"""
+Problem: Q0.50 - Basic_Level_1_Conditional_Statements_9
+Category: General
+Difficulty: Medium
+Platform: SEED-IT Platform (https://seed-it.com)
+Date Solved: 2026-09-24
+Language: python3
+Test Cases: 30 / 30 Passed (100%)
+"""
+
+num = int(input())
+for i in range(1,num+1):
+    print(i)
