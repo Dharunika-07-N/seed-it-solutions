c9f38875852db93dafbf410ae62a4a65b6b7013b
Dharunika Natarajan <dharunika0708@gmail.com>
2026-09-24 22:25:52 +0530

    Solve: [Q0.43] Basic_Level_1_Conditional_Statements_2 (Medium) - PYTHON3 | SEED-IT

---
+++ Problems/general/q0-43-basic-level-1-conditional-statements-2/solution.py
@@ -0,0 +1,17 @@
+"""
+Problem: Q0.43 - Basic_Level_1_Conditional_Statements_2
+Category: General
+Difficulty: Medium
+Platform: SEED-IT Platform (https://seed-it.com)
+Date Solved: 2026-09-24
+Language: python3
+Test Cases: 30 / 30 Passed (100%)
+"""
+
+# your code goes here
+marks = int(input())
+
+if marks>=35:
+    print("Pass")
+else:
+    print("Fail")
