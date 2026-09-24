a62b2bb2be8314684d76ee039001cc91b1745653
Dharunika Natarajan <dharunika0708@gmail.com>
2026-09-24 22:47:53 +0530

    Solve: [Q0.47] Basic_Level_1_Conditional_Statements_6 (Medium) - PYTHON3 | SEED-IT

---
+++ Problems/general/q0-47-basic-level-1-conditional-statements-6/solution.py
@@ -0,0 +1,16 @@
+"""
+Problem: Q0.47 - Basic_Level_1_Conditional_Statements_6
+Category: General
+Difficulty: Medium
+Platform: SEED-IT Platform (https://seed-it.com)
+Date Solved: 2026-09-24
+Language: python3
+Test Cases: 30 / 30 Passed (100%)
+"""
+
+# your code goes here
+char = input()
+if char.isupper():
+    print("UPPERCASE")
+else:
+    print("LOWERCASE")
