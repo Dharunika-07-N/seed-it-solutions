cf747e3aa475b1e7d60d88b422d59b8c91d523bc
Dharunika Natarajan <dharunika0708@gmail.com>
2026-09-25 20:25:02 +0530

    Solve: [Q0.51] Basic_Level_1_Conditional_Statements_10 (Medium) - PYTHON3 | SEED-IT

---
+++ Problems/general/q0-51-basic-level-1-conditional-statements-10/solution.py
@@ -0,0 +1,17 @@
+"""
+Problem: Q0.51 - Basic_Level_1_Conditional_Statements_10
+Category: General
+Difficulty: Medium
+Platform: SEED-IT Platform (https://seed-it.com)
+Date Solved: 2026-09-25
+Language: python3
+Test Cases: 30 / 30 Passed (100%)
+"""
+
+# your code goes here
+
+n = int(input())
+if n & 1:
+    print("Odd")
+else:
+    print("Even")
